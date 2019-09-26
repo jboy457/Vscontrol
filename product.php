8c7240c26a483199e64d6152ea094ec0bff96d13
Taiwo Adejare <adejareemma@gmail.com>
2019-09-26 14:12:33 +0100

    user login page

--- product.php
+++ product.php
@@ -4,6 +4,6 @@
 
 	echo '<h1>' . $name . '</h1>';
 
-	$question = 'What is your name:';
+	echo '<h2>What is your name: '. $name.' </h2>';
 
-	echo '<h2>' . $question . '  ' . $name.  ' </h2>'
+	echo "lorem ipsum donor laporea";
